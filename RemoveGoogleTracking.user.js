099e689359ff600a8c521597bb4321eacf23f999
n <>
2017-07-21 16:35:41 +0900

    Tiding over for Google Maps

--- RemoveGoogleTracking.user.js
+++ RemoveGoogleTracking.user.js
@@ -8,7 +8,7 @@
 
 // @homepageURL    https://github.com/sceneq/RemoveGoogleTracking
 
-// @version        0.7
+// @version        0.8
 // @include        https://www.google.*/*
 // @grant          none
 // @run-at         document-start
@@ -20,6 +20,7 @@
 // ==/UserScript==
 
 'use strict';
+console.time('init');
 
 try {
 	window = window.unsafeWindow || window;
@@ -30,62 +31,121 @@ const yesman = function() {
 };
 const tired = function() {};
 
+/* Return the current Google search mode */
+function getParam(parameter, name) {
+	var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(parameter);
+	if (results === null) {
+		return null;
+	} else {
+		return results.pop() || 0;
+	}
+}
+
+/* return search mode */
+function getSearchMode() {
+	const parameter = location.search + location.hash;
+	return getParam(parameter, 'tbm') || 'search';
+}
+
 // matching tracking paramaters
-const badParametersNames = [
-	'biw', // offsetWidth
-	'bih', // offsetHeight
-	'ei',
-	'sa',
-	'ved',
-	'source',
-	'prmd',
-	'bvm',
-	'bav',
-	'psi',
-	'stick',
-	'dq',
-	'ech',
-
+const badParametersNamesObj = {
+	base: [
+		'biw', // offsetWidth
+		'bih', // offsetHeight
+		'ei',
+		'sa',
+		'ved',
+		'source',
+		'prmd',
+		'bvm',
+		'bav',
+		'psi',
+		'stick',
+		'dq',
+		'ech',
+		'gs_gbg',
+		'gs_rn',
+		'cp'
+	],
 	// image search
-	'scroll',
-	'vet',
-	'yv',
-	'ijn',
-	'iact',
-	'forward',
-	'ndsp',
-	'csi',
-	'tbnid',
-	'docid',
-	//'imgdii',  // related images
+	isch: [
+		'scroll',
+		'vet',
+		'yv',
+		'ijn',
+		'iact',
+		'forward',
+		'ndsp',
+		'csi',
+		'tbnid'
+		//'docid',   // related images
+		//'imgdii',  // related images
+	],
+	searchform: [
+		// search form
+		'pbx',
+		'dpr',
+		'pf',
+		'gs_rn',
+		'gs_mss',
+		'pq',
+		'cp',
+		'oq',
+		'sclient',
+		'gs_l',
+		'aqs'
+		//'gs_ri',   // suggestions
+		//'gs_id',   // suggestions
+		//'xhr',     // suggestions at image search
+		//'tch',     // js flag?
+	],
+	// Google maps
+	maps: ['psi']
+};
 
-	// search form
-	'pbx',
-	'dpr',
-	'pf',
-	'gs_rn',
-	'gs_mss',
-	'pq',
-	'cp',
-	'oq',
-	'sclient',
-	'gs_l',
-	'aqs',
-	//'gs_ri',   // suggestions
-	//'gs_id',   // suggestions
-	//'xhr',     // suggestions at image search
-	//'tch',     // js flag?
+// search option on first access
+const GoogleOp = {
+	maps: Symbol(),
+	isch: Symbol(),
+	search: Symbol(),
+	unknown: Symbol()
+};
 
-	// mobile
-	'gs_gbg',
-	'gs_rn',
-	'cp'
-];
+const op = (() => {
+	if (location.pathname.startsWith('/maps')) {
+		return GoogleOp.maps;
+	}
+	if (getSearchMode() === 'isch') {
+		return GoogleOp.isch;
+	} else {
+		return GoogleOp.search;
+	}
+})();
+
+const badParametersNames = (() => {
+	switch (op) {
+		case GoogleOp.maps:
+			return badParametersNamesObj.maps;
+		case GoogleOp.isch:
+			return badParametersNamesObj.base.concat(
+				badParametersNamesObj.searchform,
+				badParametersNamesObj.isch
+			);
+		case GoogleOp.search:
+			return badParametersNamesObj.base.concat(
+				badParametersNamesObj.searchform
+			);
+	}
+})();
+
 const badAttrNamesObj = {
 	default: ['onmousedown', 'ping', 'oncontextmenu'],
 	search: ['onmousedown', 'ping', 'oncontextmenu'],
 	vid: ['onmousedown'],
-	isch: []
+	nws: ['onmousedown'],
+	bks: [],
+	isch: [],
+	shop: []
 };
 
 // From the nodes selected here, delete parameters specified by badParametersNames
@@ -127,31 +187,12 @@ const dirtyLinkSelector = dirtyLinkSelectors
 	.map(s => s + ":not([href=''])")
 	.join(',');
 
-/*
- * Functions
- */
 /* Return parameter value */
 function extractDirectLink(str, param) {
 	//(?<=q=)(.*)(?=&)/
 	const res = new RegExp(`[?&]${param}(=([^&#]*))`).exec(str);
 	if (!res || !res[2]) return '';
 	return decodeURIComponent(res[2]);
-}
-
-/* Return the current Google search mode */
-function getParam(parameter, name) {
-	var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(parameter);
-	if (results === null) {
-		return null;
-	} else {
-		return results.pop() || 0;
-	}
-}
-
-/* return search mode */
-function getMode() {
-	const parameter = location.search + location.hash;
-	return getParam(parameter, 'tbm') || 'search';
 }
 
 function sleep(ms) {
@@ -252,11 +293,65 @@ function load() {
 		}
 	}
 
+	const specProcesses = {
+		shop: function() {
+			// Overwrite links(desktop version only)
+			//Object.values(google.pmc.smpo.r).map(s=>{return {title:s[14][0],link:s[28][8]}})
+			if (legacy) return;
+			onDeclare(google, 'pmc.spop.r').then(shopObj => {
+				const shopElements = $$('#rso a.pstl');
+				const shopImgElements = $$('#rso a.psliimg');
+				const shopArrays = Object.values(shopObj);
+				const shopLinks = shopArrays.map(a => a[34][6]);
+				const zip = rows => rows[0].map((_, c) => rows.map(row => row[c]));
+
+				if (shopElements.length !== shopLinks.length) {
+					console.warn(
+						'length does not match',
+						shopElements.length,
+						shopLinks.length
+					);
+					return;
+				}
+
+				for (const detail of zip([
+					shopElements,
+					shopLinks,
+					shopImgElements,
+					shopArrays
+				])) {
+					// for (shopElements, shopLinks, shopImgElements, shopArrays) in zip(~)
+					const shopElement = detail[0];
+					const shopLink = detail[1];
+					const shopImgElement = detail[2];
+					const shopArray = detail[3];
+
+					// Overwrite link
+					shopElement.href = shopImgElement.href = shopLink;
+
+					// Disable click actions
+					//detail[0].__jsaction = null;
+					//detail[0].removeAttribute("jsaction");
+
+					// Overwrite variables used when link clicked
+					try {
+						shopArray[3][0][1] = shopLink;
+						shopArray[14][1] = shopLink;
+						shopArray[89][16] = shopLink;
+						shopArray[89][18][0] = shopLink;
+						shopArray[85][3] = shopLink;
+					} catch (e) {}
+				}
+				console.log('Links Rewrited');
+			});
+		}
+	};
+
 	function removeTracking() {
 		console.time('removeTracking');
-		const mode = getMode();
-		const badAttrNames = badAttrNamesObj[mode]
-			? badAttrNamesObj[mode]
+		const searchMode = getSearchMode();
+		const badAttrNames = badAttrNamesObj[searchMode]
+			? badAttrNamesObj[searchMode]
 			: badAttrNamesObj['default'];
 		const directLinkParamName = 'q';
 
@@ -282,63 +377,11 @@ function load() {
 			}
 			searchResult.href = searchResult.href.replace(regBadParameters, '');
 		}
+
 		removeBadParameters();
 
-		switch (mode) {
-			case 'shop':
-				// Overwrite links(desktop version only)
-				//Object.values(google.pmc.smpo.r).map(s=>{return {title:s[14][0],link:s[28][8]}})
-				if (legacy) break;
-				onDeclare(google, 'pmc.spop.r').then(shopObj => {
-					const shopElements = $$('#rso a.pstl');
-					const shopImgElements = $$('#rso a.psliimg');
-					const shopArrays = Object.values(shopObj);
-					const shopLinks = shopArrays.map(a => a[34][6]);
-					const zip = rows => rows[0].map((_, c) => rows.map(row => row[c]));
+		searchMode in specProcesses && specProcesses[searchMode]();
 
-					if (shopElements.length !== shopLinks.length) {
-						console.warn(
-							'length does not match',
-							shopElements.length,
-							shopLinks.length
-						);
-						return;
-					}
-
-					for (const detail of zip([
-						shopElements,
-						shopLinks,
-						shopImgElements,
-						shopArrays
-					])) {
-						// for (shopElements, shopLinks, shopImgElements, shopArrays) in zip(~)
-						const shopElement = detail[0];
-						const shopLink = detail[1];
-						const shopImgElement = detail[2];
-						const shopArray = detail[3];
-
-						// Overwrite link
-						shopElement.href = shopImgElement.href = shopLink;
-
-						// Disable click actions
-						//detail[0].__jsaction = null;
-						//detail[0].removeAttribute("jsaction");
-
-						// Overwrite variables used when link clicked
-						try {
-							shopArray[3][0][1] = shopLink;
-							shopArray[14][1] = shopLink;
-							shopArray[89][16] = shopLink;
-							shopArray[89][18][0] = shopLink;
-							shopArray[85][3] = shopLink;
-						} catch (e) {}
-					}
-					console.log('Links Rewrited');
-				});
-				break;
-			default:
-				break;
-		}
 		console.timeEnd('removeTracking');
 	}
 
@@ -409,7 +452,6 @@ function load() {
 		startObserve($('#search'), ObserveOp.CHANGE.PAGE, removeTracking);
 	}
 
-	const initMode = getMode();
 	const confDeepObserve = { childList: true, subtree: true };
 
 	// Wait for .hdtb-mn-cont appears in the first page access
@@ -419,7 +461,7 @@ function load() {
 			ObserveOp.LOADED.HDTB,
 			() => {
 				// hdtb loaded
-				switch (initMode) {
+				switch (getSearchMode()) {
 					case 'isch': // Image Search
 						removeTracking();
 
@@ -474,7 +516,6 @@ function load() {
 					s.stopPropagation();
 				}
 			};
-			console.log('FUCK');
 			document.addEventListener('click', stopAddParams, true);
 			document.addEventListener('touchStart', stopAddParams, true);
 		}
@@ -493,7 +534,6 @@ function load() {
 }
 
 function init() {
-	console.time('init');
 	onDeclare(window, 'google', 20).then(() => {
 		rewriteProperties([
 			[google, 'log', yesman],
@@ -510,7 +550,8 @@ function init() {
 	});
 
 	// Reject Request by img tag
-	const regBadImageSrc = /\/(?:gen(?:erate)?|client|fp)_204|metric\.gstatic\.com/;
+	//maps:log204
+	const regBadImageSrc = /\/(?:(?:gen(?:erate)?|client|fp)_|log)204|(?:metric|csi)\.gstatic\./;
 	Object.defineProperty(window.Image.prototype, 'src', {
 		set: function(url) {
 			if (!regBadImageSrc.test(url)) {
@@ -529,18 +570,27 @@ function init() {
 	// hook XHR
 	const origOpen = XMLHttpRequest.prototype.open;
 	window.XMLHttpRequest.prototype.open = function(act, path) {
-		if (regBadPaths.test(path)) {
-			return;
+		if (!regBadPaths.test(path)) {
+			origOpen.apply(this, [act, path.replace(regBadParameters, '')]);
 		}
-		origOpen.apply(this, [act, path.replace(regBadParameters, '')]);
 	};
-
-	console.timeEnd('init');
 }
 
 /* Execute */
 
 init();
+console.timeEnd('init');
+
+if (
+	location.pathname !== '/' &&
+	location.pathname !== '/search' &&
+	document.querySelector('html').getAttribute('itemtype') !==
+		'http://schema.org/SearchResultsPage'
+) {
+	console.warn('');
+	return;
+}
+
 window.addEventListener('DOMContentLoaded', load);
 
 // for older browser
